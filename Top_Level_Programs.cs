dbc62dccc9d3a384f5b544bc496bcbc2b6ef05bc
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-01-04 14:30:22 +0530

    Add files via upload

---
+++ Top_Level_Programs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.WriteLine("Welcome to C# 9 ");
+Console.WriteLine("Please enter the counter :-");
+int n = int.Parse(Console.ReadLine());
+
+Calculation(n);
+
+void Calculation(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        Console.WriteLine($"{n} * {i} = {n*i}");
+    }
+}
+
+Console.ReadLine();
